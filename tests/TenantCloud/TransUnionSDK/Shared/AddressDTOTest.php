eaa7d0c942b311b38c8aceadd1fd1514059b3586
Oleksandr Prypkhan <o.prypkhan@tenantcloud.com>
2022-04-11 16:13:45 +0300

    fix: Replace slashes in address because TU doesn't support them

--- tests/TenantCloud/TransUnionSDK/Shared/AddressDTOTest.php
+++ tests/TenantCloud/TransUnionSDK/Shared/AddressDTOTest.php
@@ -10,6 +10,20 @@ use Tests\TenantCloud\TransUnionSDK\TestCase;
  */
 class AddressDTOTest extends TestCase
 {
+	public function testReplacesSpecialCharactersInAddressLines(): void
+	{
+		$address = AddressDTO::create()
+			->setAddressLine1('1018 1/2 Church St')
+			->setAddressLine2('202-55')
+			->setLocality('Milton')
+			->setRegion('WV');
+
+		self::assertSame('1018 1 2 Church St', $address->getAddressLine1());
+		self::assertSame('202 55', $address->getAddressLine2());
+		self::assertSame('Milton', $address->getLocality());
+		self::assertSame('WV', $address->getRegion());
+	}
+
 	public function testTransliteratesAddressLinesCityAndState(): void
 	{
 		$address = AddressDTO::create()

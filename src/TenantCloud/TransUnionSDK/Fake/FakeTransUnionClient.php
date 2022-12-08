bae93163aebf29fa83ace514f1eba4302e75ec0f
Oleksandr Prypkhan <o.prypkhan@tenantcloud.com>
2022-12-08 12:27:36 +0200

    fix: Cyclic dependency

--- src/TenantCloud/TransUnionSDK/Fake/FakeTransUnionClient.php
+++ src/TenantCloud/TransUnionSDK/Fake/FakeTransUnionClient.php
@@ -2,22 +2,44 @@
 
 namespace TenantCloud\TransUnionSDK\Fake;
 
+use Illuminate\Contracts\Cache\Repository;
+use Illuminate\Contracts\Events\Dispatcher;
+use Illuminate\Filesystem\Filesystem;
 use TenantCloud\TransUnionSDK\Client\TransUnionClient;
+use TenantCloud\TransUnionSDK\Verification\TestModeVerificationAnswersFactory;
 
 /**
  * Fake TU client that works in-memory and doesn't perform any requests to TU.
  */
 final class FakeTransUnionClient implements TransUnionClient
 {
+	private FakeExamsApi $examsApi;
+
+	private FakeLandlordsApi $landlordsApi;
+
+	private FakeRentersApi $rentersApi;
+
+	private FakePropertiesApi $propertiesApi;
+
+	private FakeRequestsApi $requestsApi;
+
+	private FakeReportsApi $reportsApi;
+
+	private FakeTokensApi $tokensApi;
+
 	public function __construct(
-		private readonly FakeExamsApi $examsApi,
-		private readonly FakeLandlordsApi $landlordsApi,
-		private readonly FakeRentersApi $rentersApi,
-		private readonly FakePropertiesApi $propertiesApi,
-		private readonly FakeRequestsApi $requestsApi,
-		private readonly FakeReportsApi $reportsApi,
-		private readonly FakeTokensApi $tokensApi,
+		Dispatcher $eventDispatcher,
+		Filesystem $filesystem,
+		TestModeVerificationAnswersFactory $testModeVerificationAnswersFactory,
+		Repository $cache,
 	) {
+		$this->examsApi = new FakeExamsApi($this, $testModeVerificationAnswersFactory, $cache);
+		$this->landlordsApi = new FakeLandlordsApi();
+		$this->rentersApi = new FakeRentersApi($this, $cache);
+		$this->propertiesApi = new FakePropertiesApi();
+		$this->requestsApi = new FakeRequestsApi(new FakeRequestRentersApi($this, $cache));
+		$this->reportsApi = new FakeReportsApi($this, $eventDispatcher, $filesystem);
+		$this->tokensApi = new FakeTokensApi();
 	}
 
 	/**
